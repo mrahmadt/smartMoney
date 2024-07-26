c7da4653ad894a1f6a7d49f468a51f937839e13b
Ahmad <ahmadt@gmail.com>
2024-07-27 02:56:04 +0300

    .

--- resources/views/misc/nav.blade.php
+++ resources/views/misc/nav.blade.php
@@ -2,11 +2,9 @@
 <div class="cursor-pointer bg-gray-50 pb-2 mb-2 hidden" id="webpush-button">
     <div class="bg-indigo-600 text-white text-sm rounded-xl text-center mx-2 px-2 py-2 shadow-2xl">Subscribe Push Notification</div>
 </div>
-@if (Request::is('alerts*'))
 <div class="cursor-pointer bg-gray-50 pb-2 mb-2 hidden" id="disable-webpush">
     <div class="bg-indigo-600 text-white text-sm rounded-xl text-center mx-2 px-2 py-2 shadow-2xl">Unsubscribe Push Notification</div>
 </div>
-@endif
     <div class="order-3 w-full md:w-auto md:order-2">
         <div class="grid grid-cols-3 text-sm text-center divide-x">
             @if (Request::is('budgets*'))
@@ -21,7 +19,6 @@
             @endif
             @if (Request::is('alerts*'))
                 <div class="px-4 py-4 text-white bg-indigo-800 font-bold rounded"><a href="/alerts">Alerts</a></div>
-
             @else
             <a href="/alerts"><div class="px-4 py-4 text-white bg-indigo-600">Alerts</div></a>
             @endif
