6081ed1d43da1b0425e345dcb287efbf0be21392
Ahmad <ahmadt@gmail.com>
2026-04-03 02:59:52 +0000

    moving code from smartMoney to root

--- smartMoney/resources/views/filament/pages/dashboard.blade.php
+++
@@ -1,3 +0,0 @@
-<x-filament-panels::page>
-    {{-- Page content --}}
-</x-filament-panels::page>
--- smartMoney/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\SMSController;
-
-Route::post('/sms/filter', [SMSController::class, 'store']);
-
